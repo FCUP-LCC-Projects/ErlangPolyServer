cc973cbab6793ca260f63eff07ea85c80947ccd6
mbcsalmeida <61744828+mbcsalmeida@users.noreply.github.com>
2022-03-29 23:25:45 +0100

    Distributed system using Book's lib_chan modules

--- middle_server.erl
+++ middle_server.erl
@@ -1,22 +1,22 @@
 -module(middle_server).
 -export([start/3]).
 
-start(MM) ->
+start(MM, ArgC, ArgS) ->
     loop(MM).
 
 loop(MM) ->
     receive
 	{chan, MM, {add, M, S}} ->
-	    polynomial:add(M, S),
+	    MM ! {send, polynomial:add(M, S)},
 	    loop(MM);
   {chan, MM, {sub, M, S}} ->
-      polynomial:sub(M, S),
+      MM ! {send, polynomial:sub(M, S)},
       loop(MM);
   {chan, MM, {mult, M, S}} ->
-      polynomial:mult(M, S),
+      MM ! {send, polynomial:mult(M, S)},
 	    loop(MM);
   {chan, MM, {stop}} ->
-      polynomial:stop();
+      MM ! {send, polynomial:stop()};
 	{chan_closed, MM} ->
 	    true
     end.
